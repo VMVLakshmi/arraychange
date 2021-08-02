ce4296fbe733ccbe9a400ca09cca65e4c07c0f26
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-02 18:48:55 +0530

    2nd commit
    Upload all files

---
+++ collections3/src/Arraychamge.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Arraychamge {
+
+	public static void main(String[] args) {
+		ArrayList<String>names=new ArrayList<String>();
+		names.add("ravi");
+		names.add("arun");
+		names.add("rahul");
+		names.set(0,"teja");
+		System.out.println(names);
+		
+		
+
+	}
+
+}
